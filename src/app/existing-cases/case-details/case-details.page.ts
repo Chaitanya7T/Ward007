0e5941ceb84c034345018d8dab923848c7870215
Mohan <Mohana.krishnachar@rle.co.in>
2021-09-04 11:48:51 +0530

    API

--- src/app/existing-cases/case-details/case-details.page.ts
+++ src/app/existing-cases/case-details/case-details.page.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController, NavParams } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { ModalController, NavParams } from "@ionic/angular";
 
 @Component({
-  selector: 'app-case-details',
-  templateUrl: './case-details.page.html',
-  styleUrls: ['./case-details.page.scss'],
+  selector: "app-case-details",
+  templateUrl: "./case-details.page.html",
+  styleUrls: ["./case-details.page.scss"],
 })
 export class CaseDetailsPage implements OnInit {
   cases;
-  constructor(public viewCtrl: ModalController,navParams: NavParams) { 
-    this.cases=navParams.get('cases');
-    console.log("cases",this.cases)
+  constructor(public viewCtrl: ModalController, navParams: NavParams) {
+    this.cases = navParams.get("cases");
+    console.log("cases", this.cases);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   dismiss() {
     this.viewCtrl.dismiss();
   }
